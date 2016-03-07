6e4c4c8cc3a90e3c13b1d50c18f0d8aff65f6b22
heshashawky <heshamwebdeve@gmail.com>
2016-03-07 21:43:32 +0200

    new update

--- assets/js/template.js
+++ assets/js/template.js
@@ -24,4 +24,21 @@ angular.module("rootCave", [])
         return {
             templateUrl: 'assets/html/footer.html'
         };
+    }).service('aboutService', function ($http, $q) {
+        "use strict";
+        var deferred = $q.defer();
+        $http.get('assets/data.json').then(function (rcdata) {
+            deferred.resolve(rcdata);
+        });
+        this.getAbout = function () {
+            return deferred.promise;
+        };
+    }).controller('aboutCtrl', function ($scope, aboutService) {
+        var promise = aboutService.getAbout();
+        promise.then(function (rcdata) {
+            $scope.about = rcdata.data.about;
+            $scope.products = rcdata.data.products;
+            console.log($scope.about);
+            console.log($scope.products);
+        });
     });
