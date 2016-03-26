839c814195fd20b60adc90d63cfef53f211d455c
Mahmoud El Kotoury <melkotoury@gmail.com>
2016-03-26 12:32:49 +0200

    added some seo friendly tags and google analytics code

--- includes/head.php
+++ includes/head.php
@@ -3,11 +3,21 @@
 
 
     <head>
+
+        <!---- title ---->
+        <title>Root Cave | Software House </title>
+
+        <!---- title ---->
         <!-- meta tags -->
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="Description" content="Root Cave is a Software House based in Egypt , delivering state of art line of business mobile and web application.
+
+Root Cave has it's own cloud based ERP and CRM Solutions together with cloud based School and Medical Management Systems.">
+        <meta name="Keywords" content="erp,crm,mobile development, cloud erp , cloud erp companies, cheap cloud erp ,medical software, hospital management system , school management systems , schools softwares ,android development companies, mobile development companies, erp demo , software demo">
+
         <!-- meta tags -->
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -17,9 +27,7 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-        <!---- title ---->
-        <title>RootCave// SoftWare-House</title>
-        <!---- title ---->
+
 
         <!---- syles ---->
         <link rel="stylesheet" href="<?php echo $css; ?>bootstrap.min.css">
@@ -32,3 +40,12 @@
         <link rel="stylesheet" href="<?php echo $css; ?>bootstrap-formhelpers-countries.flags.css">
     </head>
     <body data-ng-controller="aboutCtrl">
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PJ58ZG"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PJ58ZG');</script>
+    <!-- End Google Tag Manager -->
